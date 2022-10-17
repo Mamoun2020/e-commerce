465ca3643bbc4774fa3070aebb9bc10f6268fcf5
Mamoun Abu Salah <70561744+Mamoun2020@users.noreply.github.com>
2022-10-17 13:51:16 +0300

    auth form laravel/ui from this comment 1.composer require laravel/ui 2.php artisan ui bootstrap --auth

--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,18 @@
 <?php
 
 use App\Http\Controllers\Admin\AdminController;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
 
 Route::prefix('admin')->name('admin.')->group(function (){
      Route::get('/',[AdminController::class,'index'])->name('dashboard');
 });
+
+Route::get('/',function (){
+   return 'Home';
+});
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
